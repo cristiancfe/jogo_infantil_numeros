4fcc31fce23adad466f790807750898f8723023d
Cristian fabiano Eisenhut <cristiancfe@gmail.com>
2024-02-19 17:59:36 -0300

    jogo numeros infantil

---
+++ app.js
@@ -0,0 +1,40 @@
+function clique1(){
+    document.getElementById("numero1").src = "./img/um.png";
+    document.getElementById("resposta1").innerHTML =  "UM";
+}
+function clique2(){
+    document.getElementById("numero2").src = "./img/dois.png";
+    document.getElementById("resposta2").innerHTML =  "DOIS";
+}
+function clique3(){
+    document.getElementById("numero3").src = "./img/tres.png";
+    document.getElementById("resposta3").innerHTML =  "TRÊS";
+}
+function clique4(){
+    document.getElementById("numero4").src = "./img/quatro.png";
+    document.getElementById("resposta4").innerHTML =  "QUATRO";
+}
+function clique5(){
+    document.getElementById("numero5").src = "./img/cinco.png";
+    document.getElementById("resposta5").innerHTML =  "CINCO";
+}
+function clique6(){
+    document.getElementById("numero6").src = "./img/seis.png";
+    document.getElementById("resposta6").innerHTML =  "SEIS";
+}
+function clique7(){
+    document.getElementById("numero7").src = "./img/sete.png";
+    document.getElementById("resposta7").innerHTML =  "SETE";
+}
+function clique8(){
+    document.getElementById("numero8").src = "./img/oito.png";
+    document.getElementById("resposta8").innerHTML =  "OITO";
+}
+function clique9(){
+    document.getElementById("numero9").src = "./img/nove.png";
+    document.getElementById("resposta9").innerHTML =  "NOVE";
+}
+function clique10(){
+    document.getElementById("numero10").src = "./img/dez.png";
+    document.getElementById("resposta10").innerHTML =  "DEZ";
+}
